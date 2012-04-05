2d9a9e2e932cc0dae785bb0eea5884898772dcab
Moritz Heidkamp <moritz@doo.net>
2012-04-05 19:19:24 +0200

    remove explicit clojure dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clj-pubnub "0.0.1-SNAPSHOT"
+(defproject clj-pubnub "0.0.1"
   :description "Clojure client for PubNub"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.3.2"]
+  :dependencies [[clj-http "0.3.2"]
                  [cheshire "2.2.2"]
                  [digest "1.4.0"]]
   :dev-dependencies [[swank-clojure "1.4.0"]])
