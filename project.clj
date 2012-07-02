14f2c21b06540e9f9e1565a37ee1b52fcdc73de2
James Thornton <james.thornton@gmail.com>
2012-07-02 09:00:19 -0500

    fixed digest vertsion 1.4.0

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Clojure client for PubNub"
   :dependencies [[clj-http "0.4.3"]
                  [cheshire "4.0.0"]
-                 [digest "1.4.2"]]
+                 [digest "1.4.0"]]
   :dev-dependencies [[swank-clojure "1.4.2"]])
